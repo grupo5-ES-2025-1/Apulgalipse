964f3c4d5451faa88939d472b080f115ca2bf545
nathantrajano <nathan.amaro.trajano@ccc.ufcg.edu.br>
2025-07-26 19:50:47 -0300

    prefabs atualizados e gameOver implementado
    sistema de vida atualizado
    game over acontece ao colidir 5 vezes com as pulgas. ajustavel.

--- Assets/Scripts/ataque.cs
+++ Assets/Scripts/ataque.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 public class ataque : MonoBehaviour
 {
 
@@ -9,13 +10,16 @@ public class ataque : MonoBehaviour
     public float ataqueRange = 0.5f;
     public LayerMask enemyLayers;
 
+    public int vida = 5;
+
     void Update()
     {
 
     }
     public void PerformAttack(InputAction.CallbackContext context)
     {
-        if (context.performed && !isAttacking) {
+        if (context.performed && !isAttacking)
+        {
             animator.SetTrigger("Attack");
             Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(ataquePoint.position, ataqueRange, enemyLayers);
             foreach (Collider2D enemy in hitEnemies)
@@ -33,4 +37,15 @@ public class ataque : MonoBehaviour
         Gizmos.DrawWireSphere(ataquePoint.position, ataqueRange);
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("enemy"))
+        {
+            vida--;
+            if (vida <= 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // Reload the current scene
+            }
+        }
+    }
 }
