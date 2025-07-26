bddc8cfbb6eb9ab5c56430cddd6eeff395562da7
nathantrajano <nathan.amaro.trajano@ccc.ufcg.edu.br>
2025-07-26 13:29:01 -0300

    animação de movimentação do player implementada

--- Assets/Scripts/movimentacaoPlayer.cs
+++ Assets/Scripts/movimentacaoPlayer.cs
@@ -3,10 +3,24 @@ using UnityEngine.InputSystem;
 public class movimentacaoPlayer : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb;
+    public float speed = 3f;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
 
+    private int isMovingHash = Animator.StringToHash("isMoving");
     public void OnMove(InputAction.CallbackContext context)
     {
         Vector2 input = context.ReadValue<Vector2>();
-        rb.linearVelocity = input * 5f; // Adjust speed as necessary
+        if (input.x < 0)
+        {
+            spriteRenderer.flipX = true; // Flip sprite for left movement
+        }
+        else if (input.x > 0)
+        {
+            spriteRenderer.flipX = false; // Unflip sprite for right movement
+        }
+        animator.SetBool(isMovingHash, input != Vector2.zero);
+        rb.linearVelocity = input * speed; // Adjust speed as necessary
+
     }
 }
