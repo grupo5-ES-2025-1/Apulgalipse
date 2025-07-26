b5d046b36abd7e86a9515a9fd484f1c9bbc0a03b
nathantrajano <nathan.amaro.trajano@ccc.ufcg.edu.br>
2025-07-26 15:40:47 -0300

    script de ataque implementado
    primeiro sistema de vida implementado tambem, porem muito rudimentar

---
+++ Assets/Scripts/ataque.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class ataque : MonoBehaviour
+{
+
+    public Animator animator;
+    private bool isAttacking;
+    public Transform ataquePoint;
+    public float ataqueRange = 0.5f;
+    public LayerMask enemyLayers;
+
+    void Update()
+    {
+
+    }
+    public void PerformAttack(InputAction.CallbackContext context)
+    {
+        if (context.performed && !isAttacking) {
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(ataquePoint.position, ataqueRange, enemyLayers);
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                // Assuming the enemy has a method to take damage
+                enemy.GetComponent<Enemy>().TakeDamage(1); // Adjust damage as necessary
+            }
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        if (ataquePoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(ataquePoint.position, ataqueRange);
+    }
+
+}
