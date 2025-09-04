bd92d0976434891e887efa8b38cd6046f91739fa
GbrlGrmno <gabri1020m@gmail.com>
2025-09-04 12:52:57 -0300

    feat: Add new items and improve inventory system
    This commit adds several new items to the game and implements the functionality to stack and drop them.
    - Implemented item stacking logic, allowing multiple items of the same type to occupy a single slot.
    - Added a way for players to drop items from their inventory, which instantiates a loot object in the world.
    - Introduced new item types: [Coxinha, Energy Drink, Umbrella].

--- Assets/Scripts/Inventory & Shop/InventoryManager.cs
+++ Assets/Scripts/Inventory & Shop/InventoryManager.cs
@@ -1,5 +1,6 @@
 using System.Runtime.CompilerServices;
 using TMPro;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -10,6 +11,8 @@ public class InventoryManager : MonoBehaviour
     public int gold;
     public TMP_Text goldText;
     public UseItem useItem;
+    public GameObject lootPrefab;
+    public Transform player;
 
     private void Start()
     {
@@ -37,21 +40,66 @@ public class InventoryManager : MonoBehaviour
             goldText.text = gold.ToString();
             return;
         }
-        else
+
+        foreach (var slot in itemSlots)
         {
-            foreach (var slot in itemSlots)
+            if (slot.itemSO == itemSO && slot.quantity < itemSO.stackSize)
             {
-                if (slot.itemSO == null)
-                {
-                    slot.itemSO = itemSO;
-                    slot.quantity = quantity;
-                    slot.UpdateUI();
+                int availableSpace = itemSO.stackSize - slot.quantity;
+
+                int amountToAdd = Mathf.Min(availableSpace, quantity);
+
+                slot.quantity += amountToAdd;
+                quantity -= amountToAdd;
+
+                slot.UpdateUI();
+
+                if (quantity <= 0)
                     return;
-                }
+
+
             }
         }
 
+        foreach (var slot in itemSlots)
+        {
+            if (slot.itemSO == null)
+            {
+                int amountToAdd = Mathf.Min(itemSO.stackSize - quantity);
 
+                slot.itemSO = itemSO;
+                slot.quantity = quantity;
+                slot.UpdateUI();
+                return;
+            }
+        }
+
+        if (quantity > 0)
+        {
+            DropLoot(itemSO, quantity);
+        }
+        
+
+
+    }
+
+    public void DropItem(InventorySlot slot)
+    {
+        DropLoot(slot.itemSO, 1);
+        slot.quantity--;
+
+        if (slot.quantity <= 0)
+        {
+            slot.itemSO = null;
+        }
+
+        slot.UpdateUI();
+    }
+
+    private void DropLoot(ItemSO itemSO, int quantity)
+    {
+        Loot loot = Instantiate(lootPrefab, player.position, Quaternion.identity).GetComponent<Loot>();
+        loot.Initialize(itemSO, quantity);
     }
 
     public void UseItem(InventorySlot slot)
@@ -62,6 +110,7 @@ public class InventoryManager : MonoBehaviour
             useItem.ApplyItemEffects(slot.itemSO);
 
             slot.quantity--;
+            Debug.Log("Item quantity: " + slot.quantity);
             if (slot.quantity <= 0)
             {
                 slot.itemSO = null;
